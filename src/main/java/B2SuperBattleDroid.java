901052e26d01819bf0b8d2dea49bc6ba1d438d14
Ryan Long <52093793+sehjinryan@users.noreply.github.com>
2026-01-22 22:10:02 +0800

    Level-0: Rename, Greet, Exit

---
+++ src/main/java/B2SuperBattleDroid.java
@@ -0,0 +1,11 @@
+public class B2SuperBattleDroid {
+    public static void main(String[] args) {
+        String separator = "____________________________________________________________";
+        System.out.println(separator);
+        System.out.println("Hello! I'm B2 Super Battle Droid");
+        System.out.println("What can I do for you?");
+        System.out.println(separator);
+        System.out.println("Bye. hope to see you again soon!");
+        System.out.println(separator);
+    }
+}
