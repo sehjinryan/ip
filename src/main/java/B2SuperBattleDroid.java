c62fd3d4bed2e196c1931a2199b90308fc83095f
Ryan Long <52093793+sehjinryan@users.noreply.github.com>
2026-01-23 10:23:55 +0800

    Level-1: Echo

--- src/main/java/B2SuperBattleDroid.java
+++ src/main/java/B2SuperBattleDroid.java
@@ -1,11 +1,30 @@
+import java.util.Scanner;
+
 public class B2SuperBattleDroid {
+    private static final String SEPARATOR = "____________________________________________________________";
+
     public static void main(String[] args) {
-        String separator = "____________________________________________________________";
-        System.out.println(separator);
+        System.out.println(SEPARATOR);
         System.out.println("Hello! I'm B2 Super Battle Droid");
         System.out.println("What can I do for you?");
-        System.out.println(separator);
-        System.out.println("Bye. hope to see you again soon!");
-        System.out.println(separator);
+        System.out.println(SEPARATOR);
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("User: ");
+            String input = scanner.nextLine().trim();
+
+            if (input.equals("bye")) {
+                System.out.println(SEPARATOR);
+                System.out.println("Bye. hope to see you again soon!");
+                System.out.println(SEPARATOR);
+                break;
+            }
+
+            System.out.println(SEPARATOR);
+            System.out.println(input);
+            System.out.println(SEPARATOR);
+        }
     }
 }
