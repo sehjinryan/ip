1b9ea2449c99574780101627b6f2115edca50ae7
Ryan Long <52093793+sehjinryan@users.noreply.github.com>
2026-01-23 10:40:00 +0800

    Level-2: Add, List

--- src/main/java/B2SuperBattleDroid.java
+++ src/main/java/B2SuperBattleDroid.java
@@ -1,7 +1,9 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class B2SuperBattleDroid {
     private static final String SEPARATOR = "____________________________________________________________";
+    private static final ArrayList<String> tasks = new ArrayList<>();
 
     public static void main(String[] args) {
         System.out.println(SEPARATOR);
@@ -15,6 +17,15 @@ public class B2SuperBattleDroid {
             System.out.print("User: ");
             String input = scanner.nextLine().trim();
 
+            if (input.equals("list")) {
+                System.out.println(SEPARATOR);
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println((i + 1) + ". " + tasks.get(i));
+                }
+                System.out.println(SEPARATOR);
+                continue;
+            }
+
             if (input.equals("bye")) {
                 System.out.println(SEPARATOR);
                 System.out.println("Bye. hope to see you again soon!");
@@ -23,8 +34,9 @@ public class B2SuperBattleDroid {
             }
 
             System.out.println(SEPARATOR);
-            System.out.println(input);
+            System.out.println("added: " + input);
             System.out.println(SEPARATOR);
+            tasks.add(input);
         }
     }
 }
