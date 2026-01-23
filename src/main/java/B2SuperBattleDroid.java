0e2ba413d4c3e8853028809e9be409106b5ee457
Ryan Long <52093793+sehjinryan@users.noreply.github.com>
2026-01-23 11:19:45 +0800

    Level-3: Mark as Done

--- src/main/java/B2SuperBattleDroid.java
+++ src/main/java/B2SuperBattleDroid.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class B2SuperBattleDroid {
     private static final String SEPARATOR = "____________________________________________________________";
-    private static final ArrayList<String> tasks = new ArrayList<>();
+    private static final ArrayList<Task> tasks = new ArrayList<Task>();
 
     public static void main(String[] args) {
         System.out.println(SEPARATOR);
@@ -19,10 +19,40 @@ public class B2SuperBattleDroid {
 
             if (input.equals("list")) {
                 System.out.println(SEPARATOR);
+                System.out.println("Here are the tasks in your list: ");
                 for (int i = 0; i < tasks.size(); i++) {
-                    System.out.println((i + 1) + ". " + tasks.get(i));
+                    Task curr = tasks.get(i);
+                    System.out.println((i + 1) + ". " + curr.getStatusIcon() + " " + curr.description);
                 }
                 System.out.println(SEPARATOR);
+                continue;
+            }
+
+            if (input.startsWith("mark")) {
+                String[] components = input.split(" ");
+                int task_id = Integer.parseInt(components[1]) - 1;
+                Task target = tasks.get(task_id);
+                target.markAsDone();
+
+                System.out.println(SEPARATOR);
+                System.out.println("Nice! I've marked this task as done: ");
+                System.out.println("  " + target.getStatusIcon() + " " + target.description);
+                System.out.println(SEPARATOR);
+
+                continue;
+            }
+
+            if (input.startsWith("unmark")) {
+                String[] components = input.split(" ");
+                int task_id = Integer.parseInt(components[1]) - 1;
+                Task target = tasks.get(task_id);
+                target.markAsUndone();
+
+                System.out.println(SEPARATOR);
+                System.out.println("OK, I've marked this task as not done yet: ");
+                System.out.println("  " + target.getStatusIcon() + " " + target.description);
+                System.out.println(SEPARATOR);
+
                 continue;
             }
 
@@ -33,10 +63,14 @@ public class B2SuperBattleDroid {
                 break;
             }
 
-            System.out.println(SEPARATOR);
-            System.out.println("added: " + input);
-            System.out.println(SEPARATOR);
-            tasks.add(input);
+            else {
+                Task t = new Task(input);
+                tasks.add(t);
+
+                System.out.println(SEPARATOR);
+                System.out.println("added: " + input);
+                System.out.println(SEPARATOR);
+            }
         }
     }
 }
